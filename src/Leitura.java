479a769888920f64da0d9fe2bc9343f6d5f45b4e
rafaelfernandes16 <rafaelfersantos16@gmail.com>
2025-11-12 16:11:22 -0300

    1,2

--- src/Leitura.java
+++ src/Leitura.java
@@ -3,6 +3,20 @@ import java.util.Scanner;
 public class Leitura {
     static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+
+        System.out.println("Digiti seu filme favorito ?");
+        String filme = leitura.nextLine();
+        System.out.println("Qual ano de lançamento ?");
+        int anoDeLançamento = leitura.nextInt();
+        System.out.println("Diga sua avaliação para o filme!");
+        double avaliacao = leitura.nextDouble();
+
+
+        System.out.println(filme);
+        System.out.println(anoDeLançamento);
+        System.out.println(avaliacao);
+
     }
+
 
 }
